c86c11f126810c47a005b76f51f811a45fb41cfd
zhaodongpo <zhaodongpo@baidu.com>
2019-02-01 14:15:24 +0800

    improve

--- 20190131/982_Triples_with_Bitwise_AND_Equal_To_Zero.py
+++ 20190131/982_Triples_with_Bitwise_AND_Equal_To_Zero.py
@@ -7,10 +7,16 @@ class Solution(object):
         """
 
         ret = 0
+        two_dic = {}
         for i in range(0, len(A)):
-            for j in range(0, len(A)):
-                for k in range(0, len(A)):
-                    if A[i] & A[j] & A[k] == 0:
-                        ret += 1
+            for j in range(i, len(A)):
+                if A[i] & A[j] not in two_dic:
+                    two_dic[A[i] & A[j]] = 0
+                two_dic[A[i] & A[j]] += 1
+                    
+        for v in two_dic:
+            for k in range(0, len(A)):
+                    if v & A[k] == 0:
+                        ret += two_dic[v]
         return ret
 
