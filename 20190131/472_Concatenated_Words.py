efae513e3f3cdf101a5f20eac9e06d34b337f744
zhaodongpo <zhaodongpo@baidu.com>
2019-02-03 15:46:48 +0800

    fix

--- 20190131/472_Concatenated_Words.py
+++ 20190131/472_Concatenated_Words.py
@@ -4,15 +4,6 @@ Date: 2019/02/03 13:37:16
 """
 
 class Solution(object):
-    def check(self, words, word, position):
-        if position == len(word):
-            return 0
-        for i in range(position + 1, len(word) + 1):
-            if word[position : i] in words:
-                num = self.check(words, word, i) 
-                if num >= 0:
-                    return num + 1
-        return -1
 
     def findAllConcatenatedWordsInADict(self, words):
         """
@@ -20,9 +11,19 @@ class Solution(object):
         :rtype: List[str]
         """
         ans = []
+        words_set = set(words)
+
+        def check(word, position):
+            if position == len(word):
+                return 0
+            for i in range(position + 1, len(word) + 1):
+                if word[position : i] in words_set:
+                    num = check(word, i) 
+                    if num >= 0:
+                        return num + 1
+            return -1
         for word in words:
-            print self.check(words, word, 0) 
-            if self.check(words, word, 0) > 1:
+            if check( word, 0) > 1:
                 ans.append(word)
         return ans 
 
