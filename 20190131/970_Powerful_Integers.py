cf64ec963e1430fcf5e4429ccce586c731ab5df7
zhaodongpo <zhaodongpo@rrx360.com>
2019-02-05 23:52:17 +0800

    fix

--- 20190131/970_Powerful_Integers.py
+++ 20190131/970_Powerful_Integers.py
@@ -7,7 +7,7 @@ class Solution(object):
         :rtype: List[int]
         """
         ret = []
-        if x == 1 amd y == 1:
+        if x == 1 and y == 1:
             return [2]
         x_power  = 1
         while x_power < bound:
@@ -15,9 +15,16 @@ class Solution(object):
             while x_power + y_power <= bound:
                 ret.append(x_power + y_power)
                 y_power *= y
-                if y_power == y:
+                if y == 1:
                     break
             x_power *= x
-            if x_power == x:
+            if x == 1 :
                 break
         return list(set(ret))
+
+if __name__ == '__main__':
+    solution = Solution()
+    print solution.powerfulIntegers(1, 1, 10)
+    print solution.powerfulIntegers(1, 2, 100)
+    print solution.powerfulIntegers(2, 3, 10)
+    print solution.powerfulIntegers(3, 5, 15)
