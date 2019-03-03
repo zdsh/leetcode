d622204f65630bc4e8a56a59fd153ef614c5e0a7
zhaodongpo <zhaodongpo@baidu.com>
2019-03-03 19:16:59 +0800

    fix

--- 20190131/761_Special_Binary_String.py
+++ 20190131/761_Special_Binary_String.py
@@ -8,10 +8,10 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-        i, res = 0, []
+        i, count, res = 0, 0, []
         for k, s in enumerate(S):
             count = count + 1 if s == '1' else count - 1
             if count == 0:
                 res.append('1' + self.makeLargestSpecial(S[i+1:k]) + '0')
-                i = k + 1 
-        return ''.join(sorted(res))
+                i = k + 1
+        return ''.join(sorted(res)[::-1]) 
