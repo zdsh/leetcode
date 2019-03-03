29bf446c0fd34f7e7cc6b8e17581d5c347ffde24
zhaodongpo <zhaodongpo@baidu.com>
2019-03-03 19:14:58 +0800

    761

---
+++ 20190131/761_Special_Binary_String.py
@@ -0,0 +1,17 @@
+"""
+File: 761_Special_Binary_String.py
+Date: 2019/03/03 13:15:51
+"""
+class Solution(object):
+    def makeLargestSpecial(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        i, res = 0, []
+        for k, s in enumerate(S):
+            count = count + 1 if s == '1' else count - 1
+            if count == 0:
+                res.append('1' + self.makeLargestSpecial(S[i+1:k]) + '0')
+                i = k + 1 
+        return ''.join(sorted(res))
