ad23c86f9186d82925790f22a8e4724a11e3b5e2
zhaodongpo <zhaodongpo@baidu.com>
2019-03-18 13:53:57 +0800

    709

---
+++ 20190131/709_To_Lower_Case.py
@@ -0,0 +1,11 @@
+"""
+File: 709_To_Lower_Case.py
+Date: 2019/03/18 13:46:43
+"""
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
