81cf5b00ab30d2be552e083a455079710019fe5a
zhaodongpo <zhaodongpo@baidu.com>
2019-04-11 21:28:40 +0800

    701

---
+++ 20190131/701_Insert_into_a_Binary_Search_Tree.py
@@ -0,0 +1,26 @@
+"""
+File: 701_Insert_into_a_Binary_Search_Tree.py
+Date: 2019/04/11 21:19:26
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+         
