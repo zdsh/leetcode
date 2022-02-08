de74ea799986d9bbb602e1141ce3556a37f26555
zdsh <zhaodongpo@gmail.com>
2022-02-08 20:26:21 +0800

    998. Maximum Binary Tree II

---
+++ 998.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def insertIntoMaxTree(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            return TreeNode(val)
+        if val > root.val:
+            node = TreeNode(val, left=root)
+            return node
+        root.right = self.insertIntoMaxTree(root.right, val)
+        return root        
